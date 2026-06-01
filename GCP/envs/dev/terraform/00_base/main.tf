8333a23d0f6e8bb3011b9a19dda24941fcba7b48
SergioM <sermariz93@gmail.com>
2026-06-01 19:36:22 +0200

    Use 'serel' GCS bucket and add product GET endpoint
    Update Terraform GCS backend bucket from "bucket-terraform-state-dp3" to "serel" across dev env modules (00_base, 01_data, 02_app) including remote_state configs. Replace list endpoint /getArticulos with /products/{id} in server/main.py: change response_model to single ArticuloOut, query by articulo_id, return 404 if not found, and improve error logging.

--- GCP/envs/dev/terraform/00_base/main.tf
+++ GCP/envs/dev/terraform/00_base/main.tf
@@ -2,7 +2,7 @@
 # This block points the Terraform state to the bucket created in GCP
 terraform {
   backend "gcs" {
-    bucket  = "bucket-terraform-state-dp3"
+    bucket  = "serel"
     prefix  = "terraform/state/base"            
   }
 }
--- GCP/envs/dev/terraform/01_data/main.tf
+++ GCP/envs/dev/terraform/01_data/main.tf
@@ -2,7 +2,7 @@
 # This block points the Terraform state to the bucket created in GCP
 terraform {
   backend "gcs" {
-    bucket  = "bucket-terraform-state-dp3"
+    bucket  = "serel"
     prefix  = "terraform/state/data"            
   }
 }
@@ -12,7 +12,7 @@ data "terraform_remote_state" "base" {
   backend = "gcs"
   
   config = {
-    bucket = "bucket-terraform-state-dp3" # El nombre de tu bucket de estados
+    bucket = "serel" # El nombre de tu bucket de estados
     prefix = "terraform/state/base"
   }
 }
